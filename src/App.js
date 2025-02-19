24cc618cc8a910b71793841324652cf641f7ffbb
Anastasy T <lvjasha@gmail.com>
2025-02-19 16:44:33 +0300

    Приложение Todo

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
+
+function App() {
+    const [todos, setTodos] = useState([]);
+    const addTodoHandler = (text) => {
+        const newTodo = {
+            text: text,
+            isCompleted: false,
+            id: uuidv4(),
+        };
+        setTodos([...todos, newTodo]);
+    };
+
+    const deleteTodoHandler = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    const toggleTodoHandler = (id) => {
+        setTodos(
+            todos.map((todo) => {
+                return todo.id === id
+                    ? { ...todo, isCompleted: !todo.isCompleted }
+                    : { ...todo };
+            })
+        );
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <TodoForm addTodo={addTodoHandler} />
+            <TodoList
+                todos={todos}
+                deleteTodo={deleteTodoHandler}
+                toggleTodo={toggleTodoHandler}
+            />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/UI/Button.js
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <div>
+            <button type="submit">Button</button>
+        </div>
+    );
+}
+
+export default Button;
